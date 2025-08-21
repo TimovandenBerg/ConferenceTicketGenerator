b10812f1bcff0a5d0d270fa5c5b9454eaab40d54
Timo van den Berg <74822698+TimovandenBerg@users.noreply.github.com>
2025-08-21 11:43:45 +0200

    added utility class in pattern module

--- app/patterns.tsx
+++ app/patterns.tsx
@@ -8,42 +8,42 @@ export default function Patterns() {
         src="/patterns/pattern-lines.svg"
         width={1024}
         height={1459}
-        className={styles.lines}
+        className={`${styles.lines} ${styles.pattern}`}
         alt="lines pattern background"
       />
       <Image
         src="/patterns/pattern-squiggly-line-bottom-mobile-tablet.svg"
         width={760}
         height={530}
-        className={styles.squigglyLineBottomMobileTablet}
+        className={`${styles.squigglyLineBottomMobileTablet} ${styles.pattern}`}
         alt="squiggly line bottom pattern background"
       />
       <Image
         src="/patterns/pattern-squiggly-line-bottom-desktop.svg"
         width={825}
         height={400}
-        className={styles.squigglyLineBottomDesktop}
+        className={`${styles.squigglyLineBottomDesktop} ${styles.pattern}`}
         alt="squiggly line bottom pattern background"
       />
       <Image
         src="/patterns/pattern-squiggly-line-top.svg"
         width={446}
         height={208}
-        className={styles.lineTop}
+        className={`${styles.lineTop} ${styles.pattern}`}
         alt="squiggly line top right pattern background"
       />
       <Image
         src="/patterns/pattern-circle.svg"
         width={217}
         height={217}
-        className={styles.topCircle}
+        className={`${styles.topCircle} ${styles.pattern}`}
         alt="transparent top circle pattern background"
       />
       <Image
         src="/patterns/pattern-circle.svg"
         width={217}
         height={217}
-        className={styles.midCircle}
+        className={`${styles.midCircle} ${styles.pattern}`}
         alt="transparent middle circle pattern background"
       />
     </>
